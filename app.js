f04f65dca46d3b4774db3bf35297d5292852d529
kingcellstudio <kingcell.studio@gmail.com>
2026-05-16 12:56:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
 const MANYCHAT_API_KEY = process.env.MANYCHAT_API_KEY;
